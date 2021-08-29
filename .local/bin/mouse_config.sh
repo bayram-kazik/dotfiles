7c9e874bd3f09be60417eeb1841d0bee649f4ccc
bayram-kazik <48856944+bayram-kazik@users.noreply.github.com>
2021-08-29 03:03:30 +0300

    Update mouse_conf.sh to use sh instead of bash

--- .local/bin/mouse_config.sh
+++ .local/bin/mouse_config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 xinput set-prop "Logitech Wireless Mouse" "libinput Accel Speed" "-1"
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" "1"
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled Default" "0"
