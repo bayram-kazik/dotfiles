f58d3c33cb440089e8362d40eb3c87cc7c079e14
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-02-29 02:37:21 +0300

    Add lxpolkit, unclutter, eww, stalonetray and xsetroot cursor to bspwmrc

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -6,12 +6,22 @@ once() {
 	fi
 }
 
+start_bar() {
+	eww daemon && eww open bar
+	killall stalonetray
+	stalonetray -v -bg '#121212' --geometry 1x6+6+675 --max-geometry 1x6+6+675 --icon-size 28 --icon-gravity S
+}
+
 once picom
 once redshift -l 25:25
 once dunst
 once greenclip daemon
+once lxpolkit
+once unclutter
+start_bar
 
 ~/.screenlayout/two-monitors.sh
 ~/.local/bin/mouse_config.sh
 setxkbmap us,tr
+xsetroot -cursor_name left_ptr
 feh --bg-center /usr/share/backgrounds/gnome/morphogenesis-d.svg
