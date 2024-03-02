031a81fb621fbbdca26728a01731676ae1221ad7
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-03-02 11:15:11 +0300

    Seperate startbar.sh from bspwm autostart.sh

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 
 once() {
 	if ! pgrep -f $1 >/dev/null; then
@@ -6,22 +6,20 @@ once() {
 	fi
 }
 
-start_bar() {
-	eww daemon && eww open bar
-	killall stalonetray
-	stalonetray -v -bg '#121212' --geometry 1x6+6+675 --max-geometry 1x6+6+675 --icon-size 28 --icon-gravity S
-}
-
 once picom
-once redshift -l 25:25
+once redshift -l 30:30
 once dunst
 once greenclip daemon
 once lxpolkit
 once unclutter
-start_bar
+once xsettingsd
+killall -HUP xsettingsd
+once darkman run
+~/.config/bspwm/startbar.sh &
 
-~/.screenlayout/two-monitors.sh
+~/.screenlayout/one-monitor.sh
 ~/.local/bin/mouse_config.sh
 setxkbmap us,tr
 xsetroot -cursor_name left_ptr
+xset -b # disable beep sound
 feh --bg-center /usr/share/backgrounds/gnome/morphogenesis-d.svg
