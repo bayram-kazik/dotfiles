8f126ec6ab5b29a05093a1b8c4de85e188095c1b
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-03-24 10:54:03 +0300

    Improve multi monitor support for bspwm

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -11,12 +11,10 @@ once redshift -l 30:30
 once dunst
 once greenclip daemon
 once lxpolkit
-once unclutter
+once gnome-keyring-daemon --start --foreground --components=secrets
 once xsettingsd
 killall -HUP xsettingsd
 once darkman run
-
-once nextcloud
 
 ~/.config/bspwm/setupmonitors.sh
 ~/.config/bspwm/mouseconfig.sh
@@ -26,3 +24,7 @@ setxkbmap us,tr -option grp:alt_caps_toggle
 xsetroot -cursor_name left_ptr
 xset -b # disable beep sound
 feh --bg-center /usr/share/backgrounds/gnome/morphogenesis-d.svg
+
+once nm-applet
+once volumeicon
+once nextcloud
